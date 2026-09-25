2f836f33af1c5faaa9c31c9c1b95fc9a83194ac8
zSimo <sondaproject@outlook.com>
2026-09-25 17:16:44 +0200

    version z1
    added video mode, VBE loader, and fixed multiboot error

--- boot/boot.s
+++ boot/boot.s
@@ -1,25 +1,42 @@
-.set ALIGN, 1<<0
-.set MEMINFO, 1<<1
-.set FLAGS, ALIGN | MEMINFO
-.set MAGIC, 0x1BADB002
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set VIDMODE,  1<<2                  
+.set FLAGS,    ALIGN | MEMINFO | VIDMODE
+.set MAGIC,    0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
+
 .section .multiboot
 .align 4
 .long MAGIC
 .long FLAGS
-.long CHECKSUM 
+.long CHECKSUM
+
+.long 0, 0, 0, 0, 0
+
+/* Graphics Mode Preferences */
+.long 0 
+.long 1024
+.long 768   
+.long 32    
+
 .section .bss
 .align 16
 stack_bottom:
 .skip 16384
 stack_top:
+
 .section .text
 .global _start
 .type _start, @function
 _start:
-mov $stack_top, %esp 
-call kernel_main
-cli
-1: hlt
-jmp 1b
+    mov $stack_top, %esp
+
+    push %ebx
+    push %eax
+
+    call kernel_main
+
+    cli
+1:  hlt
+    jmp 1b
 .size _start, . - _start
